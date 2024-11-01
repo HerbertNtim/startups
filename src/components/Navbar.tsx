ff58d47167e4b80fea789f6ccab26d374be6d534
Herbert <herbertntim15@gmail.com>
2024-11-01 10:02:35 +0000

    theme and local font added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className="px-5 py-3 bg-white shadow-md">
+    <header className="px-5 py-3 bg-white shadow-md font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image src={"/logo.png"} alt="logo" width={144} height={30} />
