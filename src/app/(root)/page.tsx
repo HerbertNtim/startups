f08eb68fc5d18709ad262f1bdb93014c75b6dc55
Herbert <herbertntim15@gmail.com>
2024-11-01 13:06:42 +0000

    starting the startup cards

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,8 @@
-const Home = () => {
+import SearchForm from "@/components/SearchForm"
+
+const Home = async ({ searchParams }: { searchParams: Promise<{query?: string}>}) => {
+  const query = (await searchParams).query ?? ""
+
   return (
     <section className="pink_container">
       <div className="relative small-heading">
@@ -12,6 +16,8 @@ const Home = () => {
 
       <h1 className="heading">Pitch Your Startup, <br /> Connect with Entrepreneur</h1>
       <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+
+      <SearchForm query={query} />
     </section>
   )
 }
